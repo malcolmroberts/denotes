6daa22c10e600ca82d3a781bba5fdce5e71f701a
Malcolm Roberts <malcolmiwroberts@gmail.com>
2010-11-10 20:34:40 +0000

    minor

--- mtex.sh
+++ mtex.sh
@@ -2,14 +2,14 @@
 
 # make two copies of the lab manual: a TA one and a student one.
 
-sed 's/setcounter{showsolutions}{0}/setcounter{showsolutions}{1}/g' 201.tex > /tmp/201.tex
-mv /tmp/201.tex .
+# make TA version:
+./sTA.sh
 pdflatex 201.tex
 pdflatex 201.tex
 cp 201.pdf ta201.pdf
 
-sed 's/setcounter{showsolutions}{1}/setcounter{showsolutions}{0}/g' 201.tex > /tmp/201.tex
-mv /tmp/201.tex .
+# make student version:
+./sStu.sh
 pdflatex 201.tex
 pdflatex 201.tex
 cp 201.pdf ta201.pdf
