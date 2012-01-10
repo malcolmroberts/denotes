36635d914fc36fca07def7d8109e3b105be9af15
Malcolm Roberts <malcolmiwroberts@gmail.com>
2012-01-10 05:43:27 +0000

    minor

--- mtex.sh
+++ mtex.sh
@@ -4,11 +4,9 @@
 
 # make TA version:
 ./sTA.sh
-pdflatex 201.tex
-pdflatex 201.tex
+latexmk -pdf 201
 mv 201.pdf ta201.pdf 
 
 # make student version:
 ./sStu.sh
-pdflatex 201.tex
-pdflatex 201.tex
+latexmk -pdf 201
